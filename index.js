65854807fab6e241527dc8efdb67c0a53f131f85
Babisha Manandhar <babisha.manandhar@gmail.com>
2018-02-23 02:53:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
+<<<<<<< HEAD
   const rankedLists = document.querySelectorAll('.ranked-list')
   
   for (let i = 0; i < rankedLists.length; i++) {
@@ -15,11 +16,18 @@ function increaseRankBy(n) {
       for (let a = 0; a < children.length; a++) {
     children[a].innerHTML = parseInt(children[a].innerHTML)+ n 
   }
+=======
+  const rankedLists = document.getElementById('app').querySelector('ul.ranked-list li')
+  
+  for (let i = 0; i < rankedLists.length; i++) {
+    ranked.lists[i].innerHTML = (i + 1).toString()
+>>>>>>> 964bd90c157f95d792a6f9740d0c12796d1d30ad
   }
    
 }
 
 function deepestChild() {
+<<<<<<< HEAD
   let node = document.querySelector("#grand-node")
   let nextNode = node.children[0]
   
@@ -29,3 +37,7 @@ function deepestChild() {
   }
   return node
 }
+=======
+  document.querySelector("#grand-node")
+}
+>>>>>>> 964bd90c157f95d792a6f9740d0c12796d1d30ad
